3fd36f7591e83c3d8eab0ef78fa8b1db0c344247
Haris-eng7 <89679109+Haris-eng7@users.noreply.github.com>
2023-06-20 23:28:02 +0500

    Update Dinner.js

--- bootcampapp/src/Dinner.js
+++ bootcampapp/src/Dinner.js
@@ -4,7 +4,7 @@ function Dinner(props) {
   return (
     <div>
       <h1>Today we are serving chicken biryani!</h1>
-      <h1>{props.dishName}</h1>
+      <h1>also serving {props.dishName}</h1>
     </div>
   );
 }
